f8e02c4402b79d51df36afda7a3b535d99ccdd1b
azzahrafatimah <zahrai2f2zp29@gmail.com>
2021-06-04 16:43:43 +0700

    Quiz 13 bagian pertama

---
+++ app/src/main/java/com/fatimah/quiz13/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.fatimah.quiz13
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val message: String = "<html><body><h1>Welcome AndroidKotlin.com</h1></body></html>"
+        val mimeType: String = "text/html"
+        val utfType: String = "UTF-8"
+        webview.loadData(message, mimeType, utfType)
+    }
+}
